552c3375657179ee3ed3c89708bfee46ef53c053
Oleksandr Nazarov <oleksandr.nazarov@intellectsoft.net>
2020-04-02 17:57:19 +0300

    Fix main application script

--- app.php
+++ app.php
@@ -2,7 +2,7 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use SPS\Game;
+use SPS\Game\Game;
 use SPS\Player\PaperStrategyPlayerFactory;
 use SPS\Player\RandomStrategyPlayerFactory;
 use SPS\Rule\PaperHitsStoneRuleFactory;
@@ -10,13 +10,15 @@ use SPS\Rule\ScissorsHitsPaperRuleFactory;
 use SPS\Rule\StoneHitsScissorsFactory;
 use SPS\Utils;
 
-
-$playerA = PaperStrategyPlayerFactory::makePlayer();
-$playerB = RandomStrategyPlayerFactory::makePlayer();
-
-$game = new Game(100);
+echo '[GAME HAS BEEN STARTED]' . PHP_EOL . PHP_EOL;
 
 try {
+    $rounds = 100;
+    $game = new Game($rounds);
+
+    $playerA = PaperStrategyPlayerFactory::makePlayer();
+    $playerB = RandomStrategyPlayerFactory::makePlayer();
+
     $game->addPlayer($playerA);
     $game->addPlayer($playerB);
 
@@ -30,6 +32,8 @@ try {
     $playerBScore = Utils::calculatePlayerTotalScore($playerB);
 
     echo Utils::outputGameResults($playerA, $playerB) . PHP_EOL;
-} catch (\Exception $exception) {
+} catch (Exception $exception) {
     echo '[Error] ' . $exception->getMessage() . PHP_EOL;
 }
+
+echo '[GAME HAS BEEN FINISHED]' . PHP_EOL;
