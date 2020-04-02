961d8c59daa768ddb50e8950a477fb2958870f7d
Oleksandr Nazarov <oleksandr.nazarov@intellectsoft.net>
2020-04-02 17:55:13 +0300

    Add phpdoc to all methods

--- src/Player/Player.php
+++ src/Player/Player.php
@@ -36,41 +36,66 @@ class Player implements PlayerInterface
         $this->strategy = $strategy;
     }
 
+    /**
+     * @return ItemInterface
+     */
     public function play(): ItemInterface
     {
         return $this->strategy->play();
     }
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return $this->name;
     }
 
+    /**
+     * @param string $name
+     */
     public function setName(string $name): void
     {
         $this->name = $name;
     }
 
+    /**
+     * @return StrategyInterface
+     */
     public function getStrategy(): StrategyInterface
     {
         return $this->strategy;
     }
 
+    /**
+     * @param StrategyInterface $strategy
+     */
     public function setStrategy(StrategyInterface $strategy): void
     {
         $this->strategy = $strategy;
     }
 
+    /**
+     * @return array
+     */
     public function getScores(): array
     {
         return $this->scores;
     }
 
+    /**
+     * @param int $round
+     * @return Score|null
+     */
     public function getScoreForRound(int $round): ?Score
     {
         return $this->scores[$round - 1] ?? null;
     }
 
+    /**
+     * @param Score $score
+     */
     public function addScore(Score $score): void
     {
         $this->scores[] = $score;
